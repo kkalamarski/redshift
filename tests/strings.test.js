43d2f0ca1212e67054d9de5b1a6e5b48b88c70d1
Krzysztof Kalamarski <kkalamarski@gmail.com>
2018-10-23 23:35:49 +0200

    :sparkles: Add support for string literals + concatenation operator

--- tests/strings.test.js
+++ tests/strings.test.js
@@ -10,4 +10,26 @@ describe("Strings", () => {
 
     expect(result).toBe("a string")
   })
+
+  it("should concat two strings", () => {
+    const code = `
+        str = "hello " <> "world!"
+        str
+      `
+
+    const result = compile(code, true)
+    expect(result).toBe("hello world!")
+  })
+
+  it("should concat two strings from variables", () => {
+    const code = `
+        str = "hello "
+        world = "world!"
+        result = str <> world
+        result
+      `
+
+    const result = compile(code, true)
+    expect(result).toBe("hello world!")
+  })
 })

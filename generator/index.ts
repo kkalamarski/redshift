c0d4720b448a461748a4672ad3447e843c8f4c99
Krzysztof Kalamarski <kkalamarski@gmail.com>
2018-11-06 11:37:34 +0100

    :sparkles: Add Webapp to generated files

--- generator/index.ts
+++ generator/index.ts
@@ -6,11 +6,74 @@ const main = target => `import "./src/App.rh" as App
 App.main()
 `
 
-const app = target => `import IO
+const app = target => `import WebComponent
 
 defmodule App do
   def main() do
-    IO.puts("Hello world!")
+    update = fn(state) -> App.update(state) end
+    template = fn(render, state) -> App.template(render, state) end
+
+    WebComponent.of("rh-app", update, template)    
+  end
+
+  def update(state) do
+    new_state = state.set("name", "Redshift")
+
+    new_state
+  end
+
+  def template(render, state) do 
+    name = state.get("name")
+
+    render ~ "
+      <style>
+        :host {
+          font-family: 'Montserrat', sans-serif;
+          font-size: 16px;
+        }
+
+        h1 { 
+          color: #ffffff;
+          font-size: 3em;
+          text-align: center;
+          padding: 200px;
+          margin: 0;
+
+          background: #C02425;
+          background: -webkit-linear-gradient(to right, #F0CB35, #C02425); 
+          background: linear-gradient(to right, #F0CB35, #C02425);
+        }
+
+        h2 {
+          padding: 25px;
+          text-align: center;
+          font-size: 2em;
+          font-weight: normal;      
+          background: #C02425;
+          color: #FFFFFF;
+          margin: 0;
+        }
+
+        p {
+          text-align: center;
+          font-size: 1.5em;
+        }
+
+        a {
+          display: block;
+          text-align: center;
+          color: #C02425;
+          text-decoration: none;
+          font-size: 1.5em;
+        }
+
+      </style>
+      <h1>Welcome to the {{ name }} app!</h1>
+      <h2>This is a base template for Redshift web applications</h2>
+      <p>Start editing in /src/App.rh.</p>
+      <p> -- or -- </p>
+      <a href='https://github.com/kkalamarski/redshift'>Visit Github Page</a>
+    "
   end
 end
 `
@@ -26,7 +89,7 @@ const html = () => `<!DOCTYPE html>
 </head>
 
 <body>
-    <h1>Hello world!</h1>
+    <rh-app></rh-app>
 
     <script src="bin/bundle.js"></script>
 </body>

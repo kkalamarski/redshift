90e783b0a218cfafd8d18ccb2769e22840250af9
Krzysztof Kalamarski <kkalamarski@gmail.com>
2018-11-11 22:10:08 +0100

    :bookmark: Version 0.5.2 - pass event tu mutate function

--- core/WebComponent.ts
+++ core/WebComponent.ts
@@ -7,8 +7,8 @@ export default class WebComponent {
   public static html = hybrids.html
   public static svg = hybrids.svg
 
-  public static mutate = fn => state => {
-    Object.assign(state, fn(state))
+  public static mutate = fn => (state, event) => {
+    Object.assign(state, fn(state, event))
 
     return state
   }
